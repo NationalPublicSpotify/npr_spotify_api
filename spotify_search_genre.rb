6804a5bd25e389e374b12b29c71ca6b10760f14e
Scott Williams <williamsjs1224@gmail.com>
2015-05-28 22:59:13 -0400

    add search spotify by genre

--- spotify_search_genre.rb
+++ spotify_search_genre.rb
@@ -1,38 +1,19 @@
 require 'httparty'
 
 class SpotifySearchPlaylist
-  attr_reader :input
+  attr_reader :input, :path
 
   def initialize(input)
     @input = input
-
+    @path = get_path['playlists']['items'][0]['external_urls']['spotify']
   end
 
-
-
-
-  def path
+  private def get_path
     HTTParty.get("https://api.spotify.com/v1/search?q=#{input}&type=playlist")
   end
 
 
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#https://api.spotify.com/v1/search?q="doom metal"&type=playlist
+death_metal = SpotifySearchPlaylist.new('death metal')
+puts death_metal.path
