52a6e10dd0e3c2d2c0fa3ef367c65d45d2edb7fb
Scott Williams <williamsjs1224@gmail.com>
2015-05-28 23:05:37 -0400

    clean up genre playlist spotify class

--- spotify_search_genre.rb
+++ spotify_search_genre.rb
@@ -5,7 +5,15 @@ class SpotifySearchPlaylist
 
   def initialize(input)
     @input = input
-    @path = get_path['playlists']['items'][0]['external_urls']['spotify']
+    @path = get_genre_playlist
+  end
+
+  private def get_genre_playlist
+    if get_path['playlists']['items'] != []
+      get_path['playlists']['items'][0]['external_urls']['spotify']
+    else
+      'no genre available'
+    end
   end
 
   private def get_path
@@ -15,5 +23,5 @@ class SpotifySearchPlaylist
 
 end
 
-death_metal = SpotifySearchPlaylist.new('death metal')
+death_metal = SpotifySearchPlaylist.new('rock')
 puts death_metal.path
